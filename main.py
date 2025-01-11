cad0f5d164b0064fc8cb253db93652365439fff0
Rooban Sappani <2000rooban@gmail.com>
2025-01-11 09:37:17 +0530

    fixed main.py bug

--- main.py
+++ main.py
@@ -19,20 +19,6 @@ def assign_vals():
     st.session_state["user_login_manager"] = UserLogin()
     st.session_state["ui_manager"] = UI(st.session_state["user_login_manager"])
 
-# Main app
-if __name__ == "__main__":
-
-    load_dotenv()
-    genai.configure(api_key=os.getenv("google_gemini_api_key"))
-    llm_model = genai.GenerativeModel("gemini-1.5-flash")
-
-    vb_manager = VectorDatabase()
-    user_login_manager = UserLogin()
-
-    ui_manager = UI(user_login_manager, vb_manager, llm_model)
-
-    ui_manager.run()
-
 def main():
 
     load_dotenv()
