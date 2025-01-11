7e1f48023af3941d096334ffbecc727f850c9061
Rooban Sappani <2000rooban@gmail.com>
2025-01-11 11:09:06 +0530

    fixed bug in initializing vars

--- main.py
+++ main.py
@@ -7,23 +7,28 @@ from vector_database import *
 from user_login_info import *
 from ui import *
 
-@st.cache_resource()
-def assign_vals():
-    st.session_state["pdfs"] = False
-    st.session_state["vb_manager"] = VectorDatabase()
+def init_session():
 
-    genai.configure(api_key=os.getenv("google_gemini_api_key"))
-    llm_model = genai.GenerativeModel("gemini-1.5-flash")
+    if "pdfs" not in st.session_state:
+        st.session_state["pdfs"] = False
+    
+    if "vb_manager" not in st.session_state:
+        st.session_state["vb_manager"] = VectorDatabase()
 
-    st.session_state["llm_model"] = llm_model
-    st.session_state["user_login_manager"] = UserLogin()
-    st.session_state["ui_manager"] = UI(st.session_state["user_login_manager"])
-    print("assigned values")
+    if "llm_model" not in st.session_state:
+        genai.configure(api_key=os.getenv("google_gemini_api_key"))
+        st.session_state["llm_model"] = genai.GenerativeModel("gemini-1.5-flash")
+
+    if "user_login_manager" not in st.session_state:
+        st.session_state["user_login_manager"] = UserLogin()
+    
+    if "ui_manager" not in st.session_state:
+        st.session_state["ui_manager"] = UI(st.session_state["user_login_manager"])
 
 def main():
 
     load_dotenv()
-    assign_vals()
+    init_session()
     print("this is a test message")
 
     st.session_state["ui_manager"].run()
